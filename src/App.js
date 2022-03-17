9a6f40a237e56d1fc1cc952f24eaebf4bcefe47f
coldpigli <piyushdas98@gmail.com>
2022-03-18 01:39:40 +0530

    Feat: Added basic routing + Initialized folder structure+ Imported Component Library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { RoutingPath } from "./configs";
 
 function App() {
   return (
     <div className="App">
       <h1>hi from app</h1>
+      <RoutingPath/>
     </div>
   );
 }
---
+++ src/configs/RoutingPath.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Routes, Route} from "react-router-dom";
+import { Home, Products, Wishlist, Cart, Login, Signup, ErrorPage } from '../pages';
+
+const RoutingPath = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Products" element={<Products/>}/>
+        <Route path="/Wishlist" element={<Wishlist/>}/>
+        <Route path="/Cart" element={<Cart/>}/>
+        <Route path="/Login" element={<Login/>}/>
+        <Route path="/Signup" element={<Signup/>}/>
+        <Route path="*" element={<ErrorPage/>}/>
+      </Routes>
+  )
+}
+
+export default RoutingPath;
